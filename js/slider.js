21f34481e6fba8181b4af50042a1150d74c610f9
Qudenaza <qudenaza@gmail.com>
2018-10-20 14:38:38 +0300

    Добавил меню для мобильной версии

--- js/slider.js
+++ js/slider.js
@@ -51,3 +51,18 @@ $('.owl-carousel').owlCarousel({
     }
   }
 });
+
+var navMain = document.querySelector('.user-nav');
+var navToggle = document.querySelector('.user-nav__toggle');
+
+navMain.classList.remove('user-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('user-nav--closed')) {
+    navMain.classList.remove('user-nav--closed');
+    navMain.classList.add('user-nav--opened');
+  } else {
+    navMain.classList.add('user-nav--closed');
+    navMain.classList.remove('user-nav--opened');
+  }
+});
