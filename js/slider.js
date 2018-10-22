f69bc6f34771017d303ffa86c907d3dd5398a21f
Qudenaza <qudenaza@gmail.com>
2018-10-22 12:30:54 +0300

    Добавил форму обратной связи

--- js/slider.js
+++ js/slider.js
@@ -66,3 +66,23 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('user-nav--opened');
   }
 });
+
+$('a[href^="#"]').bind('click.smoothscroll', function(e) {
+  e.preventDefault();
+
+  var target = this.hash,
+    $target = $(target);
+
+  $('html, body')
+    .stop()
+    .animate(
+      {
+        scrollTop: $target.offset().top
+      },
+      500,
+      'swing',
+      function() {
+        window.location.hash = target;
+      }
+    );
+});
